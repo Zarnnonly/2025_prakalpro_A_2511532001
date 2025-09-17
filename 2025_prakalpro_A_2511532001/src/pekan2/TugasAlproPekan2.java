2f64c4da5b0058a88e2866d53df011f90357c4ab
Zahrand <Zahrand@Zahrand>
2025-09-17 21:15:59 +0700

    Tugas Alpro Pekan 2

--- 2025_prakalpro_A_2511532001/src/pekan2/TugasAlproPekan2.java
+++ 2025_prakalpro_A_2511532001/src/pekan2/TugasAlproPekan2.java
@@ -1,40 +1,19 @@
 package pekan2;
 
-
-import java.util.Scanner;
-
 public class TugasAlproPekan2 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        
+    	// Deklarasi variabel
+        int level = 90;                   // level max karakter
+        float hp = 40150.7f;              // contoh HP Zhongli
+        char rarity = '5';                // bintang 5
+        boolean statusOnline = true;      // apakah sedang dimainkan
 
-        // Input data
-        System.out.print("Masukkan NIM: ");
-        String nim = input.nextLine();
-
-        System.out.print("Masukkan Nama: ");
-        String nama = input.nextLine();
-
-        System.out.print("Masukkan Umur: ");
-        int umur = input.nextInt();
-
-        System.out.print("Masukkan IPK: ");
-        double ipk = input.nextDouble();
-
-        // Mengonversikan umur menjadi String
-        String umurString = String.valueOf(umur);
-
-        // Mengonversikan NIM ke Long dan ditambah dengan umur
-        long nimLong = Long.parseLong(nim);
-        long hasilJumlah = nimLong + umur;
-
-        // Print hasil
-        System.out.println("\n=== Data Mahasiswa ===");
-        System.out.println("NIM   : " + nim);
-        System.out.println("Nama  : " + nama);
-        System.out.println("Umur  : " + umur + " tahun");
-        System.out.println("IPK   : " + ipk);
-
-        System.out.println("\nUmur (String): " + umurString);
-        System.out.println("NIM + Umur   : " + hasilJumlah);
+        // Print Output
+        System.out.println("=== Data Karakter Genshin Impact ===");
+        System.out.println("Level       : " + level);
+        System.out.println("HP          : " + hp);
+        System.out.println("Rarity      : " + rarity + "★");
+        System.out.println("Online      : " + statusOnline);
     }
 }
